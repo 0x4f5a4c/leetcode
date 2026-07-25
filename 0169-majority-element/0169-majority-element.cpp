6784cd1c2fde126b9a38a632c6741fe54d2529dc
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-25 20:22:55 +0530

    Implement majority element algorithms
    Added a brute force approach and a sorting approach for finding the majority element in the array.

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+    // guess to brute force 
+    // time complexity : O(n) + O(n)
+    // space complexity : O(n)
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mpp;  // [element, count]
+        int n = nums.size();
+        // O(n)
+        for (int i = 0; i < n; ++i) {
+            mpp[nums[i]]++;    
+        }
+
+        int ans;
+        // O(n)
+        for (auto data : mpp) {
+            if (data.second > n/2) {
+                ans = data.first;
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    // approach 2
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); // sorting takes some time sweetheart
+        int n = nums.size();
+        return nums[n/2];
+    }
+};
+
+class Solution {
+public:
     // time complexity : O(n)
     // space complexity : O(1)
     
