08da98bff1c77fdccfdecdf9b757a8561f18e9db
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-26 17:17:42 +0530

    Refactor isPalindrome to use string comparison
    Refactor isPalindrome method to use string conversion for palindrome check.

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        // basic thought
+        string str = to_string(x);
+        int l = 0, r = str.size() - 1;
+        while (l <= r) {
+            if (str[l] != str[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
         if (x < 0) return false;
 
         long reverse = 0;
