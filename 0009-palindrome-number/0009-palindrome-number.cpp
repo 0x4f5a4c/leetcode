7ab5b4a47adef4044f8e2528274c0d7135c561d2
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-26 17:15:57 +0530

    Time: 4 ms (31.26%), Space: 8.6 MB (37.7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // basic thought
-        string str = to_string(x);
-        int l = 0, r = str.size() - 1;
-        while (l <= r) {
-            if (str[l] != str[r]) return false;
-            l++;
-            r--;
+        if (x < 0) return false;
+
+        long reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
         }
-        return true;
+
+        return reverse == xcopy;
     }
 };
