79be38877817ec87797a16e971e84af678603807
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-28 14:16:21 +0530

    Implement missing number solution using XOR
    Added a second approach to find the missing number using XOR.

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,3 +1,6 @@
+// time complexity : O(N)
+// space complexity : O(1)
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -7,5 +10,22 @@ public:
         for (int i = 0; i < n; ++i) curr_sum += nums[i];
 
         return total_sum - curr_sum;
+    }
+};
+
+// approach 2 using xor (^)
+// time complexity : O(N)
+// space complexity : O(1)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n_xor = nums.size();
+        for (int i = 0; i < nums.size(); ++i) {
+            n_xor ^= i;
+            n_xor ^= nums[i];
+        }
+
+        return n_xor;
     }
 };
