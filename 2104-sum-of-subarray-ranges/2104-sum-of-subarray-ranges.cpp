eb77c63a4e429a1a26b9c87f1739485e737f7dd2
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-30 19:44:45 +0530

    Time: 15 ms (19.21%), Space: 14.1 MB (75.33%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int maxVal = nums[i], minVal = nums[i];
+
+            for (int j = i; j < n; ++j) {
+                maxVal = max(maxVal, nums[j]);
+                minVal = min(minVal, nums[j]);
+                ans += (maxVal - minVal);
+            }
+        }
+
+        return ans;
+    }
+};
