3e3bff37c8e551ad60654287c859e869fd17b3da
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-30 22:06:11 +0530

    Implement frequency count method for minimum pushes
    Added an alternative solution using frequency counting to calculate minimum pushes required to type a word.

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // time complexity : O(26/8) almost O(1)
+    // space complexity : O(1)
     int minimumPushes(string word) {
         int n = word.size();
         if (n <= 8) return n;
@@ -11,6 +13,28 @@ public:
         }
         if (rem == 0) return ans;
         ans = ans + (sz + 1) * rem;
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    // another way of solving using frequency count
+    int minimumPushes(string word) {
+        int n = word.size();
+        // this frequency also take space 
+        // O(26)
+        vector<int> freq_map(26, 0);
+        for (auto &ch : word) freq_map[ch - 'a']++;
+        // sorting will take time off course  -- O(logn)
+        sort(freq_map.begin(), freq_map.end(), greater<int>());  // sort the frequency in decending order 
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            int freq = freq_map[i];
+            int press = (i/8) + 1;
+            ans += (freq * press);
+        }
+
         return ans;
     }
 };
