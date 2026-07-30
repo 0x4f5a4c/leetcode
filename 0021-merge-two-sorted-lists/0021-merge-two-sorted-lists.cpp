1df9f7e94c46bea1f291f17534868b89d08cea2e
iamafzal-dev <iamafzal.tech@gmail.com>
2026-07-30 15:59:40 +0530

    Add mergeTwoLists function for merging sorted lists
    Implemented the mergeTwoLists function to merge two sorted linked lists into one sorted list.

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,6 +10,55 @@
  */
 class Solution {
 public:
+    ListNode* mergeTwoLists(ListNode* L1, ListNode* L2) {
+        if (!L1) return L2;
+        if (!L2) return L1;
+
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        ListNode* t1 = L1;
+        ListNode* t2 = L2;
+
+        while (t1 && t2) {
+            if (t1->val == t2->val) {
+                temp->next = t1;
+                t1 = t1->next;
+                temp = temp->next;
+                temp->next = t2;
+                t2 = t2->next;
+                temp = temp->next;
+            } else if (t1->val < t2->val) {
+                temp->next = t1;
+                t1 = t1->next;
+                temp = temp->next;
+            } else {
+                temp->next = t2;
+                t2 = t2->next;
+                temp = temp->next;
+            }
+        }
+
+        if (!t1) temp->next = t2;
+        if (!t2) temp->next = t1;
+
+        return dummyNode->next;
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if (!list1) return list2;
         if (!list2) return list1;
