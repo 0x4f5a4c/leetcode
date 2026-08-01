5ef7ebc43b48060684ecfbb2651243c34f0e1abb
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-01 14:36:45 +0530

    Optimize predictTheWinner with dynamic programming
    Refactor the predictTheWinner function to use dynamic programming for optimal performance.

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -7,6 +7,37 @@ using namespace std;
 
 class Solution {
 public:
+    int solve(int i, int j, vector<int> &nums) {
+        if (i > j) return 0;
+        if (i == j) return nums[i];
+        int take_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
+        int take_j = nums[j] + min(solve(i, j-2, nums), solve(i+1, j-1, nums));
+
+        return max(take_i, take_j);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        int total_sum = 0;
+        for (int x : nums) total_sum += x;
+
+        int player1 = solve(0, n-1, nums);
+        int player2 = total_sum - player1;
+
+        return player1 >= player2;
+    }
+};
+
+
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/predict-the-winner/description/?envType=daily-question&envId=2026-08-01
+
+class Solution {
+public:
     int t[23][23];
     int solve(int i, int j, vector<int> &nums) {
         if (i > j) return 0;
