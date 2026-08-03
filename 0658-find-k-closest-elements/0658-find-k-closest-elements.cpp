9ba821545230963ff3b25ad312903e21ab674154
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-03 13:08:04 +0530

    Optimize findClosestElements with priority queue
    Refactor findClosestElements function to use a priority queue for better performance.

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        for (int n : arr) {
+            if (k > 0) {
+                min_heap.push(n);
+                k--;
+            } else if (abs(min_heap.top() - x) > abs(n - x)) {
+                min_heap.pop();
+                min_heap.push(n);
+            }
+        }
+
+        vector<int> ans;
+        while (!min_heap.empty()) {
+            ans.push_back(min_heap.top());
+            min_heap.pop();
+        }
+
+        return ans;
+    }
+};
+
+class Solution {
+public:
     // better then before 
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int l = 0, r = arr.size() - 1;
