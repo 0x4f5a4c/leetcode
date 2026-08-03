791f90846af1d4ecd69ec3a4199fd0b56729255b
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-03 13:06:28 +0530

    Time: 3 ms (55.48%), Space: 36.5 MB (54.83%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
+    // better then before 
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<int, vector<int>, greater<int>> min_heap;
-        for (int n : arr) {
-            if (k > 0) {
-                min_heap.push(n);
-                k--;
-            } else if (abs(min_heap.top() - x) > abs(n - x)) {
-                min_heap.pop();
-                min_heap.push(n);
-            }
+        int l = 0, r = arr.size() - 1;
+        while (r - l + 1 > k) {
+            if (abs(arr[l] - x) > abs(arr[r] - x)) l++;
+            else r--;
         }
 
         vector<int> ans;
-        while (!min_heap.empty()) {
-            ans.push_back(min_heap.top());
-            min_heap.pop();
+        for (int i = l; i <= r; ++i) {
+            ans.push_back(arr[i]);
         }
 
         return ans;
