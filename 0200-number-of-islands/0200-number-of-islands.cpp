cc562418e41e3a0bbb550dee762907834b95a965
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-08 11:09:22 +0530

    Time: 18 ms (97.96%), Space: 23.2 MB (28.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
     int m, n;
-
     void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') return;
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1')
+            return;
         grid[i][j] = '$'; // mark as visited
 
         // now dfs calls
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    void bfs(vector<vector<char>>& grid, int i, int j) {
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        grid[i][j] = '$'; // mark as visited;
+
+        while (!que.empty()) {
+            auto it = que.front();
+            que.pop();
+
+            for (auto &dir : directions) {
+                int new_i = it.first + dir[0];  
+                int new_j = it.second + dir[1];
+
+                if (new_i < 0 || new_i >= m || new_j < 0 || new_j >= n ||
+                    grid[new_i][new_j] != '1')
+                    continue;
+                else {
+                    que.push({new_i, new_j});
+                    grid[new_i][new_j] = '$';
+                }
+            }
+        }
     }
 
     int numIslands(vector<vector<char>>& grid) {
@@ -20,7 +45,7 @@ public:
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     islands++;
                 }
             }
