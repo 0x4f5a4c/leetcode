c2d2b3b099d2a118db8fb4b092a7a7bc4aec50ea
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-08 12:44:10 +0530

    Time: 40 ms (5.01%), Space: 108.5 MB (9.09%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -15,15 +15,44 @@ public:
         dfs(grid, i, j - 1, m, n, peri);
     }
 
+    // using bfs
+    
+    int bfs(vector<vector<int>>& grid, int i, int j, int m, int n, vector<vector<int>> &directions) {
+        int peri = 0;
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        grid[i][j] = -1;  // mark as visited;
+
+        while (!que.empty()) {
+            auto [row, col] = que.front();
+            que.pop();
+
+            for (auto &dir : directions) {
+                int new_i = row + dir[0];
+                int new_j = col + dir[1];
+
+                if (new_i < 0 || new_i >= m || new_j < 0 || new_j >= n || grid[new_i][new_j] == 0) {
+                    peri++;
+                } else if (grid[new_i][new_j] == -1) continue;
+                else {
+                    que.push({new_i, new_j});
+                    grid[new_i][new_j] = -1;  // mark as visited
+                }
+                    
+            }
+        }
+
+        return peri;
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int peri = 0;
-
+        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};  // using bfs
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 1) {
-                    dfs(grid, i, j, m, n, peri);
-                    return peri;
+                    return bfs(grid, i, j, m, n, directions);
                 }
             }
         }    
