5ce1aaead29e68a2bd36c73e3299ca82f0fd4ae5
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-11 01:53:34 +0530

    Refactor minimumPairRemoval for better sorting
    Refactor minimumPairRemoval function to improve sorting logic and maintain pair removal count.

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -5,6 +5,42 @@
 using namespace std;
 
 class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int count = 0;
+
+        while (!is_sorted(nums.begin(), nums.end())) {
+            int n = nums.size();
+            int min_val = INT_MAX;
+            int idx = 0;
+            count++;
+
+            for (int i = 0; i < n - 1; ++i) {
+                if (nums[i] + nums[i + 1] < min_val) {
+                    min_val = nums[i] + nums[i + 1];
+                    idx = i;
+                }
+            }
+
+            vector<int> ans;
+            for (int i = 0; i < n; ++i) {
+                if (i != idx)
+                    ans.push_back(nums[i]);
+                else {
+                    ans.push_back(min_val);
+                    i++;
+                }
+            }
+
+            nums = ans;
+        }
+
+        return count;
+    }
+};
+
+// -------------------------- Another way -------------------------- //
+class Solution {
 private:
     bool isSorted(vector<int>& nums, int n) {
         for(int i = 1; i < n; i++) {
