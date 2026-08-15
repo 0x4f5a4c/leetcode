fe421c6c608b933496af2e2593447fce56aac99c
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-15 21:37:20 +0530

    Time: 167 ms (87.38%), Space: 10.6 MB (78.81%) - LeetHub

---
+++ 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.size();
+        int ans = INT_MAX;
+
+        for (int r = 0; r < n; ++r) {
+            int cost = r;
+            for (int i = 0; i < n/2; ++i) {
+                int a = s[(i + r) % n] - 'a';
+                int b = s[(n - 1 - i + r) % n] - 'a';
+                int diff = abs(a - b);
+                cost += min(diff, 26 - diff);
+            }
+            ans = min(ans, cost);
+        }
+
+        return ans;
+    }
+};
