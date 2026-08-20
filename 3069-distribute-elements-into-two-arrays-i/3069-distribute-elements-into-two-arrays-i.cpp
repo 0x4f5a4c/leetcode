20c3ba0827a31e8b1f7c2f8dcb9b17d4b4f6666f
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-20 20:32:01 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.63%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -1,9 +1,15 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/distribute-elements-into-two-arrays-i/description/?envType=daily-question&envId=2026-08-20
+
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> arr1, arr2;
-
         arr1 = {nums[0]}, arr2 = {nums[1]};
 
         for (int i = 2; i < n; ++i) {
