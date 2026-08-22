5bcbddd2c132de9c0e2db8cf9c56b170340957d2
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-22 12:04:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.55%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,30 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/?envType=daily-question&envId=2026-08-22
+
+class Solution {
+public:
+
+    pair<int, int> sum_product(int n) {
+        int sum = 0, product = 1;
+
+        for (int i = n; i > 0; i /= 10) {
+            const int rem = i % 10;
+            sum += rem;
+            product *= rem;
+        }
+
+        return {sum, product};
+    }
+
+    bool checkDivisibility(int n) {
+        int orignal_n = n;
+        auto response = sum_product(n);
+
+        int ans = response.first + response.second;
+        return (orignal_n % (ans)) == 0;
+    }
+};
