220fbf45d612cd64c8618b2a37cd2d89ce6f9e15
iamafzal-dev <iamafzal.tech@gmail.com>
2026-08-22 12:35:36 +0530

    Time: 52 ms (73.3%), Space: 182.1 MB (61.77%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,73 +1,81 @@
+
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
 class NumArray {
 public:
     int n;
     vector<int> segment_tree;
 
-    void build_tree(int i, int l, int r, vector<int> &nums) {
-        // base case
-        if (l == r) {
-            segment_tree[i] = nums[l];
-            return;
-        }
-
-        // build on left and right
-        int mid = l + (r - l) / 2;
-        build_tree(2*i+1, l, mid, nums);
-        build_tree(2*i+2, mid + 1, r, nums);
-
-        segment_tree[i] = segment_tree[2*i + 1] + segment_tree[2 * i + 2];
-    }
-
-    // update the segment tree -- point update
-    void update_tree(int index, int val, int i, int l, int r) {
+    void build_tree(int curr_idx, int left, int right, vector<int> &nums) {
         // base case 
-        if (l == r) {
-            segment_tree[i] = val;
+        if (left == right) {
+            segment_tree[curr_idx] = nums[left];
             return;
         }
 
-        int mid = l + (r - l) / 2;
-        if (index <= mid) {
-            // in the left side
-            update_tree(index, val, 2*i+1, l, mid);
-        } else {
-            update_tree(index, val, 2*i+2, mid+1, r);
-        }
+        int mid = left + (right - left) / 2;
+        build_tree(2 * curr_idx + 1, left, mid, nums);
+        build_tree(2 * curr_idx + 2, mid + 1, right, nums);
 
-        segment_tree[i] = segment_tree[2*i+1] + segment_tree[2*i+2];
+        segment_tree[curr_idx] = segment_tree[2 * curr_idx + 1] + segment_tree[2 * curr_idx + 2];
     }
 
-    // now query the tree
+    /**
+     * this is the method used to update the segment tree
+     */
 
-    int sum_query(int left, int right, int i, int l, int r) {
-        // out of bound condition
-        if (l > right || r < left) return 0;
-        if (l >= left && r <= right) {
-            // in the range
-            return segment_tree[i];
-        } else {
-            int mid = l + (r - l) / 2;
-            return sum_query(left, right, 2*i+1, l, mid) + sum_query(left, right, 2*i+2, mid+1, r);
+    void update_segment_tree(int index, int val, int curr_idx, int left, int right) {
+        // base case
+        if (left == right) {
+            segment_tree[curr_idx] = val;
+            return;
         }
+
+        // now go into left and right or waha ja ke check kro 
+        int mid = left + (right - left) / 2;
+        if (index <= mid) {  // means the value is in the left side 
+            update_segment_tree(index, val, 2 * curr_idx + 1, left, mid);
+        } else {
+            update_segment_tree(index, val, 2 * curr_idx + 2, mid + 1, right);
+        }
+
+        // now returning ke time tum yaha uppar wale values ko bhi update krte hue backtrack kro --- samjhe 
+        segment_tree[curr_idx] = segment_tree[2 * curr_idx + 1] + segment_tree[2 * curr_idx + 2];
     }
 
+    /**
+     * this function will return the sum within a range
+     * @return int
+     */
+
+    int range_sum_query(int start, int end, int curr_idx, int left, int right) {
+        // jb start or end bilkul apne aukaat ke bahar ho
+        if (left > end || right < start) return 0;
+        if (left >= start && right <= end) return segment_tree[curr_idx];  // matlab  bhai within the range hai 
+        else {
+            int mid = left + (right - left) / 2;
+            return range_sum_query(start, end, 2 * curr_idx + 1, left, mid) + range_sum_query(start, end, 2 * curr_idx + 2, mid + 1, right);
+        }
+    }
+    
     NumArray(vector<int>& nums) {
-        n = nums.size();
+        n = nums.size();  // size of the passed nums
         segment_tree.resize(4*n);
+        // now build the tree
         build_tree(0, 0, n-1, nums);
     }
     
     void update(int index, int val) {
-        update_tree(index, val, 0, 0, n-1);
+        // yaha hum update krenge value ko diye hue particular index me 
+        update_segment_tree(index, val, 0, 0, n-1);
     }
     
     int sumRange(int left, int right) {
-        return sum_query(left, right, 0, 0, n-1);
+        return range_sum_query(left, right, 0, 0, n-1);
     }
-
-    // int update_range() {
-    //     // use lazy propagation
-    // }
 };
 
 /**
