91a2e3beb54cea800a2a2df4618ea3d0b3a5fe78
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-09 09:34:33 +0530

    09-09-2026 Medium 3871-count-commas-in-range-ii .cpp Math 0 ms (100%) 9.2 MB (19.2%)

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,10 +1,17 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question Link : https://leetcode.com/problems/count-commas-in-range-ii/description/?envType=daily-question&envId=2026-09-09
+
 class Solution {
 public:
-    long long countCommas(long long n) {
-        long long count = 0;
-        for (long long p = 1000; p <= n; p *= 1000) 
+    int64_t countCommas(long long n) {
+        int64_t count = 0;
+        for (int64_t p = 1000; p <= n; p *= 1000) 
             count += n - p + 1;
-
-        return count;
+        
+            return count;
     }
 };
