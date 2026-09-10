0ad9d3ab533900ba6aba189717a42e2621c656a9
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-10 11:18:09 +0530

    09-10-2026 Medium 0137-single-number-ii .cpp Array 1 ms (44.34%) 13.4 MB (35.57%)

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,66 +1,14 @@
-// رَبِّ زِدْنِي عِلْمًا
-// اے میرے رب! میرے علم میں اضافہ فرما۔
-#include <bits/stdc++.h>
-using namespace std;
 
-/**
- * more optmize solution
- */
+// another solution using sorting 
 
-class Solution {
- public:
-  int singleNumber(vector<int>& nums) {
-    int ones = 0;
-    int twos = 0;
-
-    for (const int num : nums) {
-      ones ^= (num & ~twos);
-      twos ^= (num & ~ones);
-    }
-
-    return ones;
-  }
-};
-
-/**
- * efficient solution
- */
-
- class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        for (int bit_idx = 0; bit_idx <= 31; ++bit_idx) {
-            int count = 0;
-            for (int i = 0; i <= n-1; ++i) {
-                if (nums[i] & (1 << bit_idx)) count++;
-            }
-
-            if (count % 3 == 1) ans |= (1 << bit_idx);
-        }
-
-        return ans;
-    }
-};
-
-// brute force using hash map
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        // <val, freq>
-        unordered_map<int, int> freq;
-        
-        for (int &x : nums) freq[x]++;
-        int ans = 0;
-        for (auto [val, frequency] : freq) {
-            if (frequency == 1) {
-                ans = val;
-                break;
-            }
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < n; i += 3) {
+            if (nums[i] != nums[i-1]) return nums[i-1];
         }
-
-        return ans;
+        return nums[n-1];
     }
 };
