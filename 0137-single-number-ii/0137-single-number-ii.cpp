89ac18a05c07e6de845d97cc63467b6b00efb188
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-10 11:01:05 +0530

    09-10-2026 Medium 0137-single-number-ii .cpp Array 3 ms (32.01%) 13.3 MB (87.67%)

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,22 +3,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * efficient solution
+ */
 
-// brute force using hash map
-class Solution {
+ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        // <val, freq>
-        unordered_map<int, int> freq;
-        
-        for (int &x : nums) freq[x]++;
         int ans = 0;
-        for (auto [val, frequency] : freq) {
-            if (frequency == 1) {
-                ans = val;
-                break;
+        for (int bit_idx = 0; bit_idx <= 31; ++bit_idx) {
+            int count = 0;
+            for (int i = 0; i <= n-1; ++i) {
+                if (nums[i] & (1 << bit_idx)) count++;
             }
+
+            if (count % 3 == 1) ans |= (1 << bit_idx);
         }
 
         return ans;
