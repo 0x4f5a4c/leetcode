f3903a5f97ee34e0e5ec6ac9d469ec4c62fd739e
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-10 11:07:53 +0530

    09-10-2026 Medium 0137-single-number-ii .cpp Array 0 ms (100%) 13.4 MB (35.57%)

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,23 +4,19 @@
 using namespace std;
 
 /**
- * efficient solution
+ * more optmize solution
  */
 
- class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        for (int bit_idx = 0; bit_idx <= 31; ++bit_idx) {
-            int count = 0;
-            for (int i = 0; i <= n-1; ++i) {
-                if (nums[i] & (1 << bit_idx)) count++;
-            }
+class Solution {
+ public:
+  int singleNumber(vector<int>& nums) {
+    int ones = 0;
+    int twos = 0;
 
-            if (count % 3 == 1) ans |= (1 << bit_idx);
-        }
-
-        return ans;
+    for (const int num : nums) {
+      ones ^= (num & ~twos);
+      twos ^= (num & ~ones);
     }
+    return ones;
+  }
 };
