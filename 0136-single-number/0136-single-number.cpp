e3e3e0f39f64f1edd19480a25da7d35dd07f045a
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-10 10:33:35 +0530

    Add brute force solution for single number problem
    Implemented a brute force solution to find the single number in a vector using a frequency map.

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -18,3 +18,22 @@ public:
         return ans;
     }
 };
+
+// brute force -- first thought
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> freq;  // <nums, freq>
+
+        for (int i = 0; i < n; ++i) {
+            freq[nums[i]]++;
+        }
+
+        for (auto [elem, freqency] : freq) {
+            if (freqency == 1) return elem;
+        }
+
+        return -1;
+    }
+};
