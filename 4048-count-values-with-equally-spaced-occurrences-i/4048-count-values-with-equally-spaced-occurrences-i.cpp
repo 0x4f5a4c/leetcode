4dfdddf3ac47ac6f3408fdb31f2703b7646a7dcb
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-12 21:35:18 +0530

    09-12-2026 Easy 4048-count-values-with-equally-spaced-occurrences-i .cpp UNKNOWN 19 ms (24.53%) 34 MB (33.39%)

---
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        
+        unordered_map<int, vector<int>> positions;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            positions[nums[i]].push_back(i);
+        }
+
+        int ans = 0;
+
+        for (auto &it : positions) {
+            vector<int> pos = it.second;
+            if (pos.size() != 3) {
+                continue;
+            }
+
+            int gap = pos[1] - pos[0];
+            bool special = true;
+
+            for (int i = 2; i < pos.size(); ++i) {
+                if (pos[i] - pos[i-1] != gap) {
+                    special = false;
+                    break;
+                }
+            }
+
+            if (special) ans++;
+        }
+
+        return ans;
+    }
+};
