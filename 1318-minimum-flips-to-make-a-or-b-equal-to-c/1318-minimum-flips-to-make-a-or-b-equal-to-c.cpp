6327a57065b0d9b24bbdfbc9e9f8562765dc472f
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-14 17:32:42 +0530

    Add two approaches for minimum flips calculation
    Implemented two approaches to calculate minimum flips required to make either 'a' or 'b' equal to 'c'. The first approach uses bitwise operations in a loop, while the second utilizes built-in functions for counting set bits.

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,3 +1,43 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// approach one
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int flips = 0;
+        while (a != 0 || b != 0 || c != 0) {
+            if ((c & 1) == 1) {
+                // get the right most bit
+                if ((a & 1) == 0 && (b & 1) == 0) flips++;
+            } else {
+                if ((a & 1) == 1) flips++;
+                if ((b & 1) == 1) flips++;
+
+                // flips += ((a & 1) == 1) + ((b & 1) == 1);
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return flips;
+    }
+};
+
+// another approach 
+
+int count_setbit(int n) {
+    int count = 0;
+    while (n != 0) {
+        n = n & (n-1);
+        count++;
+    }
+
+    return count;
+}
 
 class Solution {
 public:
