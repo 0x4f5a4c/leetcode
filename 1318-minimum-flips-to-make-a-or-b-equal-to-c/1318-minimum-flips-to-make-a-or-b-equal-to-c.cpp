41553c1cc7c04216204a5b53ca9fef9f3d8dfae5
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-14 17:31:53 +0530

    09-14-2026 Medium 1318-minimum-flips-to-make-a-or-b-equal-to-c .cpp Bit Manipulation 3 ms (24.62%) 7.7 MB (92.74%)

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,30 +1,8 @@
-// رَبِّ زِدْنِي عِلْمًا
-// اے میرے رب! میرے علم میں اضافہ فرما۔
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flips = 0;
-        while (a != 0 || b != 0 || c != 0) {
-            if ((c & 1) == 1) {
-                // get the right most bit
-                if ((a & 1) == 0 && (b & 1) == 0)
-                    flips++;
-            } else {
-                if ((a & 1) == 1)
-                    flips++;
-                if ((b & 1) == 1)
-                    flips++;
-
-                // flips += ((a & 1) == 1) + ((b & 1) == 1);
-            }
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
-        }
-
-        return flips;
+        int res = ((a | b) ^ c);
+        return __builtin_popcount(res) + __builtin_popcount((a & b) & res);
     }
 };
