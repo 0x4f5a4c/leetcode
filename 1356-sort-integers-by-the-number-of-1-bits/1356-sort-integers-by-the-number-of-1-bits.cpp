03892a47a28be3d6ab58cdd90df3860525490f1d
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-15 14:23:16 +0530

    Implement bit counting and sorting by number of 1 bits
    Added a function to count the number of 1 bits in an integer and updated the sorting logic to use this function.

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -3,7 +3,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// better then before 
+class Solution {
+public:
+    int count_one_bits(int num) {
+        int count = 0;
+        while (num != 0) {
+            count += (num & 1);
+            num >>= 1;
+        }
+
+        return count;
+    }
+
+    vector<int> sortByBits(vector<int>& arr) {
+        auto lambda_function = [&](int &a, int &b) {
+            int count_one_bits_in_A = count_one_bits(a);
+            int count_one_bits_in_B = count_one_bits(b);
+
+            if (count_one_bits_in_A == count_one_bits_in_B) return a < b;
+            return count_one_bits_in_A < count_one_bits_in_B;
+        };
+
+        sort(arr.begin(), arr.end(), lambda_function);
+        return arr;
+    }
+};
+
+// another solution using built_in function
+
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
