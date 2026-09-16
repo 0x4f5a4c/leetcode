394d5755cfa310e866f0478ae5821a393c0fbd65
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-17 02:24:53 +0530

    09-17-2026 Medium 0049-group-anagrams .cpp Array 12 ms (82.93%) 24.9 MB (82.95%)

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,27 @@
+// رَبِّ زِدْنِي عِلْمًا
+// اے میرے رب! میرے علم میں اضافہ فرما۔
+#include <bits/stdc++.h>
+using namespace std;
+
+// this is my initial thoughts on this problem, I will try to solve it using hashing and sorting
+/**
+ * @time_complexity O(n * k log k) where n is the number of strings and k is the maximum length of a string
+ * @space complexity O(n * k) where n is the number of strings and k is the maximum length of a string
+ */
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        unordered_map<string, vector<string>> mpp;
-        int n = strs.size();
-        for (int i = 0; i < n; ++i) {
-            string temp = strs[i];
-            sort(temp.begin(), temp.end());
-            mpp[temp].push_back(strs[i]);
+        unordered_map<string, vector<string>> mp;
+        for (auto &s : strs) {
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
         }
 
-        for (auto x : mpp) {
-            ans.push_back(x.second);
+        for (auto &p : mp) {
+            ans.push_back(p.second);
         }
-
         return ans;
     }
 };
