ffa09a9b24e0dff70d09f013c7babb7f61a09aeb
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-21 22:34:25 +0530

    09-21-2026 Medium 0347-top-k-frequent-elements .cpp Array 0 ms (100%) 20.2 MB (5.79%)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,34 +4,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// approach 1
-// using min_heap
 class Solution {
 public:
-    typedef pair<int, int> P;
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        // using bucket sort
         int n = nums.size();
         unordered_map<int, int> mpp;
         for (int &x : nums) mpp[x]++;
-        // define min heap
-        priority_queue<P, vector<P>, greater<P>> pq;
-        // push the elements into the min_heap and maintain the k size only
-
+        vector<vector<int>> bucket(n+1);
+        // index = frequency
+        // value = element
+        // bucket[i] = elements having i frequency 
         for (auto &it : mpp) {
-            int val  = it.first;
+            int elem = it.first;
             int freq = it.second;
-
-            pq.push({freq, val});
-            if (pq.size() > k) pq.pop();
+            bucket[freq].push_back(elem);
         }
 
-        // convert the min heap into vec
-        vector<int> ans;
-        while (!pq.empty()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        // result 
+        // pick elements from right to left from bucket to find the max frequency 
+        vector<int> res;
+        for (int i = n; i >= 0; --i) {
+            if (bucket[i].size() == 0) continue;
+            while (bucket[i].size() > 0 && k > 0) {
+                res.push_back(bucket[i].back());
+                bucket[i].pop_back();
+                k--;
+            }
         }
 
-        return ans;
+        return res;
     }
 };
