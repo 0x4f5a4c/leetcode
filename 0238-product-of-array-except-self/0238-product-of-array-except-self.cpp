e5652f1836a58491fdcaa173f7bc0e59cc5bf494
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-22 02:22:32 +0530

    09-22-2026 Medium 0238-product-of-array-except-self .cpp Array 4 ms (20.98%) 40.3 MB (43.32%)

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,26 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> left_profucts(n, 1);
-        left_profucts[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            left_profucts[i] = left_profucts[i - 1] * nums[i-1];
+        vector<int> res(n, 1);
+        res[0] = 1;
+        for (int i = 1; i < n; ++i) res[i] = res[i-1] * nums[i-1];
+
+        int rp = 1;
+        for (int i = n-1; i >= 0; --i) {
+            res[i] *= rp;
+            rp *= nums[i];
         }
 
-        vector<int> right_product(n, 1);
-        right_product[n-1] = 1;
-        for (int i = n-2; i >= 0; --i) {
-            right_product[i] = right_product[i + 1] * nums[i+1];
-        }
-
-        vector<int> ans(n);
-        for (int i = 0; i < n; ++i) {
-            ans[i] = left_profucts[i] * right_product[i];
-        }
-        return ans;
+        return res;
     }
 };
