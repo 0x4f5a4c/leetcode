49f0e61682b087625240f905bf6405e270ff4906
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-21 23:30:15 +0530

    Add three approaches for top K frequent elements
    Implemented three approaches to find the top K frequent elements in a vector: brute force, min-heap, and bucket sort.

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,6 +4,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * Brute force solution
+ */
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; ++i) mpp[nums[i]]++;  // it takes O(N) time traversing and space O(N) times
+        // now transform this into a vector or pairs
+        // where pair<int, int> stores <freq, element>
+
+        vector<pair<int, int>> vec;
+        for (auto &it : mpp) {
+            vec.push_back({it.second, it.first});
+        }
+
+        sort(vec.rbegin(), vec.rend());
+        vector<int> ans;
+
+        for (int i = 0; i < k; ++i) {
+            ans.push_back(vec[i].second);
+        }
+
+        return ans;
+    }
+};
+
+// approach 1
+// using min_heap
+class Solution {
+public:
+    typedef pair<int, int> P;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        for (int &x : nums) mpp[x]++;
+        // define min heap
+        priority_queue<P, vector<P>, greater<P>> pq;
+        // push the elements into the min_heap and maintain the k size only
+
+        for (auto &it : mpp) {
+            int val  = it.first;
+            int freq = it.second;
+
+            pq.push({freq, val});
+            if (pq.size() > k) pq.pop();
+        }
+
+        // convert the min heap into vec
+        vector<int> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
+
+// using approach 2
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -36,3 +98,4 @@ public:
         return res;
     }
 };
+
