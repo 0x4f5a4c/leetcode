a3adb8693b01eea7f2d92ad9c5f162c667e201db
iamafzal-dev <iamafzal.tech@gmail.com>
2026-09-26 15:24:42 +0530

    09-26-2026 Medium 1423-maximum-points-you-can-obtain-from-cards .cpp Array 0 ms (100%) 46.4 MB (32.85%)

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,4 +1,3 @@
-
 // رَبِّ زِدْنِي عِلْمًا
 // اے میرے رب! میرے علم میں اضافہ فرما۔
 #include <bits/stdc++.h>
@@ -8,19 +7,22 @@ class Solution {
 public:
     int maxScore(vector<int>& points, int k) {
         int n = points.size();
+        if (n == 0) return 0;
         if (n == k) return accumulate(points.begin(), points.end(), 0);
 
-        int max_sum = 0;
-
-        for (int i = 0; i < k; ++i) max_sum += points[i];
+        int left_sum = 0, max_point = 0;
+        for (int i = 0; i < k; ++i) left_sum += points[i];
+        max_point = left_sum;
         
-        int curr_sum = max_sum;
-        for (int i = k - 1; i >= 0; --i) {
-            curr_sum -= points[i];
-            curr_sum += points[points.size() - k + i];
-            max_sum = max(max_sum, curr_sum);
+        int right_sum = 0, right_idx = n-1;
+        for (int i = k-1; i >= 0; --i) {
+            left_sum -= points[i];
+            right_sum += points[right_idx];
+            right_idx--;
+
+            max_point = max(max_point, left_sum + right_sum);
         }
 
-        return max_sum;
+        return max_point;
     }
 };
